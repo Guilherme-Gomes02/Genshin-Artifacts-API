25bc9ef0dba07f95dd187521120b313a3c6ec05b
Guilherme Gomes <guilhermegomesfps2010@gmail.com>
2026-06-29 16:26:29 -0300

    Melhorando tratamento de erros na API

--- app.py
+++ app.py
@@ -4,17 +4,42 @@ import streamlit as st
 st.title("genshin artefatos")
 
 def genshin_artefatos():
-    endpoint = "https://genshin.jmp.blue/artifacts"
-    response = requests.get(endpoint)
+    try:
+        endpoint = "https://genshin.jmp.blue/artifacts"
+        response = requests.get(endpoint, timeout=5)
+        response.raise_for_status()
 
-    if response.status_code == 200:
-        return response.json()
-    else: 
-        return []
+        data = response.json()
+        return {
+            "data": data,
+            "ok": True 
+                }
+    
+    except requests.exceptions.Timeout:
+        return {
+            "ok":False,
+            "error": "A API demorou demais para carregar"
+        }
+
+    except requests.exceptions.HTTPError:
+        return {
+            "ok":False,
+            "error": "A API saiu do ar"
+        }
+
+    except requests.exceptions.InvalidURL:
+        return {
+            "ok":False,
+            "error": "A URL está errada"
+        }
 
 artefatos = genshin_artefatos()
 
 st.subheader("Lista de artefatos")
 
-for artefato in artefatos:
-    st.markdown(f"- {artefato}")
+if artefatos["ok"]:
+    for artefato in artefatos["data"]:
+        st.markdown(f"- {artefato}")
+    
+else:
+    st.error(artefatos["error"])
